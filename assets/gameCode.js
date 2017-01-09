027c7b89216d1a5b59b26b24f1218b6477eca212
aaronjaymus <aaronjaymus@gmail.com>
2017-01-09 11:48:09 -0500

    Select players and will write to data base. On page load writeNames method is called and writes names to page. But when same function is called from within selectPlayer method or from directly in the on click function, the writeNames method is called, logging that is was called, but doesn't actually write the player names to page.

--- assets/gameCode.js
+++ assets/gameCode.js
@@ -12,12 +12,52 @@ var database = firebase.database();
   
 var rpsGame = {
 
-	player1: undefined,
+	localPlayer: null,
 
-	player2: undefined,
+	player1: null,
+
+	player2: null,
   	
   	selectPlayer: function (){
-  		//console.log("test");
+  
+  		console.log("selectPlayer function called");
+
+   			//check the database to see if player 1 or 2 has been selected and set the local and server variables to the player names chosen
+			if(this.localPlayer === null) {
+				
+				var userRef = firebase.database().ref("users");
+
+				userRef.once("value")
+					.then(function(snapshot){
+					console.log("P1? "+ snapshot.child("player1").exists());	
+				  	if (snapshot.child("player1").exists()) {
+				 		
+				 		rpsGame.player1 = snapshot.child("player1").val();
+				 		rpsGame.player2 = $("#player-name").val().trim();
+				 		rpsGame.localPlayer = rpsGame.player2;
+				 		
+				 		database.ref("users").update({
+				 			"player2": rpsGame.player2
+				 		});
+
+				 	console.log("Player2: "+ rpsGame.player2);
+				 	
+				 	} else {
+				 		
+				 		rpsGame.player1 = $("#player-name").val().trim();
+				 		rpsGame.localPlayer = rpsGame.player1;
+				 		
+				 		database.ref("users").update({
+				 			"player1": rpsGame.player1
+				 		});
+				 		
+				 	console.log("Player1: " + rpsGame.player1);
+				 	};
+				});
+			};
+
+		
+
   	},
 
   	selectHand: function (){
@@ -28,36 +68,103 @@ var rpsGame = {
   		//console.log("test");
   	},
 
+  	writeNames: function (){
+
+  		console.log("writeNames function called");
+
+  		var userRef = new firebase.database().ref("users");
+
+  		userRef.once("value").then(function(snapshot){
+  			
+  			//check if player 1 or 2 exists and write names to player-container
+  			if(snapshot.child("player1").exists() && snapshot.child("player2").exists()){
+  				var p1 = $("<p>");
+  				p1.html("Player 1: " + snapshot.child("player1").val());
+  				var p2 = $("<p>");
+  				p2.html("player 2: " + snapshot.child("player2").val());
+
+  				$("#player-container").empty();
+  				$("#player-container").append(p1, p2);  									
+
+  			} else if (snapshot.child("player1").exists()) {
+  				var p1 = $("<p>");
+  				p1.html("Player 1: " + snapshot.child("player1").val());
+
+  				$("#player-container").empty();
+  				$("#player-container").append(p1);
+  			} else if (snapshot.child("player2").exists()) {
+  				var p2 = $("<p>");
+  				p2.html("Player 2: " + snapshot.child("player2").val());
+
+  				$("#player-container").empty();
+  				$("#player-container").append(p2);
+  			};
+  			//check if local player has been selected and write to player-container. clear name-container if local player exists
+  			if(rpsGame.localPlayer !== null){
+  				$("#name-container").empty();
+  				if (rpsGame.localPlayer === snapshot.child("player1").val()){
+  					var p = $("<p>");
+  					p.html("You are Player 1");
+
+  					$("player-container").prepend(p);
+  				} else if (rpsGame.localPlayer === snapshot.child("player2").val()){
+  					var p = $("<p>");
+  					p.html("You are Player 2");
+
+  					$("#player-container").prepend(p);
+  				}
+  			};
+
+  		});
+  	},
+
   	writeResults: function (){
   		//console.log("test");
+  	},
+
+  	createUserGroup: function (){
+  		database.ref().once("value").then(function(snapshot){
+  			if(!snapshot.child("users").exists()){
+  				database.ref().set({
+  					"users": 0
+  				});
+  			};
+		});
+  	},
+
+  	start: function (){
+  		rpsGame.createUserGroup();
+  		rpsGame.writeNames();
+  	},
+
+  	leaveGame: function (){
+
   	}
 
  };
 
- $("submit-name").click(function(event){
+rpsGame.start();
+
+
+
+ $("#submit-name").click(function(event){
+
 
  	event.preventDefault();
 
- 	if(database.ref().player1select && database.ref().player2select){
- 		alert("Both players already selected");
- 	} else if (database.ref().player1select) {
- 		rpsGame.player2 = $("#player-name").val().trim();
- 		database.ref().set({
- 			"player2": rpsGame.player2,
- 			"player2select": true
- 		});
- 		console.log(rpsGame.player2);
- 	} else {
- 		rpsGame.player1 = $("#player-name").val().trim();
- 		database.ref().set({
- 			"player1": rpsGame.player1,
- 			"player1select": true
- 		});
- 		console.log(rpsGame.player2);
- 	}
+ 	rpsGame.selectPlayer();
+ 	rpsGame.writeNames();
+
+
  });
 
  $(".hand").click(function(){
 
  });
+
+/*
+ $(window).unload(function(){
+
+ });
+*/
 
